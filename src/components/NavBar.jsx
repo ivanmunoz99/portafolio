6da338aa711f2eb27183df98e3badd285862e03c
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-02-26 18:36:09 -0500

    section description

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import Logo from '../assets/img/logo-negativo-web.png'
 function NavBar() {
     return (
         <nav class="navbar navbar-expand-lg bg-primary ">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">PORTAFOLIO</a>
+            <div class="container-fluid px-5">
+                <a class="navbar-brand text-white" href="/">PORTAFOLIO</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -13,13 +13,13 @@ function NavBar() {
                 <img src={Logo} class="rounded mx-auto d-block" alt="Logo" width={100}/>
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+                            <a class="nav-link active text-white" aria-current="page" href="/">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" href="/">Trabajos</a>
+                            <a class="nav-link active text-white" href="/">Trabajos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" href="/">CV</a>
+                            <a class="nav-link active text-white" href="/">CV</a>
                         </li>
                     </ul>
                 </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,17 +5,17 @@ function Home() {
     return (
         <div >
             <div className="container-fluid bg-primary">
-                <div className="container text-center">
-                    <p>Hola</p>
-                <p>Soy Ivan Muñoz</p>
-                <p>Soy ingeniero digital con enfoque al desarrollo web, contenido digital/multimedia y recursos informáticos.
+                <div className="container text-center text-white">
+                    <p className='fs-6 pt-5 mb-0'><small>Hola</small> </p>
+                <p className='fs-1 fw-bolder'>Soy Ivan Muñoz</p>
+                <p className=''>Soy ingeniero digital con enfoque al desarrollo web, contenido digital/multimedia y recursos informáticos.
                 </p>
-                <p>Te presento mi portafolio en el cual podrás ver mis trabajos a lo largo de mi trayectoria.
+                <p className='mb-5'>Te presento mi portafolio en el cual podrás ver mis trabajos a lo largo de mi trayectoria.
                 </p>
-                <button>descargar CV</button>
+                <button className='btn btn-outline-warning'>descargar CV</button>
                 </div>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,128L80,160C160,192,320,256,480,250.7C640,245,800,171,960,160C1120,149,1280,203,1360,229.3L1440,256L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,128L60,117.3C120,107,240,85,360,101.3C480,117,600,171,720,165.3C840,160,960,96,1080,74.7C1200,53,1320,75,1380,85.3L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
         </div>
 
     )
