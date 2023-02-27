6275fd85ad577011ed34efa4ef02e9f0e3bb9d88
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-02-26 22:17:49 -0500

    footer and redirections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
+import '../components/Footer.css'
 
 function Footer() {
     return (
         <div className='container-fluid bg-light'>
             <div className="container">
                 <p className='m-0 pt-4 text-center'>CONTACTAME</p>
-                <hr className='m-0'/>
-                <ul>
-                    <li style={{i:'#a955ff', j:'#ea51ff'}}>
-                        <span className='icon'><i className='bi bi-github'></i></span>
-                        <span className='titulo'>Github</span>
-                    </li>
-                    <li style={{i:'#a955ff', j:'#ea51ff'}}>
-                        <span className='icon'><i className='bi bi-whatsapp'></i></span>
-                        <span className='titulo'>WhatsApp</span>
-                    </li>
-                    <li style={{i:'#a955ff', j:'#ea51ff'}}>
-                        <span className='icon'><i className='bi bi-linkedin'></i></span>
-                        <span className='titulo'>Linkedin</span>
-                    </li>
-                </ul>
+                <hr className='m-0' />
+                <div className="contact">
+                    <ul>
+                        <a href="https://github.com/ivanmunoz99" target="blank">
+                            <li>
+                                <span className='icon'><i className='bi bi-github'></i></span>
+                                <span className='titulo'>Github</span>
+                            </li>
+                        </a>
+                        <a href="https://api.whatsapp.com/send/?phone=573043506199&text=" target="blank">
+                            <li>
+                                <span className='icon'><i className='bi bi-whatsapp'></i></span>
+                                <span className='titulo'>WhatsApp</span>
+                            </li>
+                        </a>
+                        <a href="www.linkedin.com/in/ivan-muñoz-andrade" target="blank">
+                            <li>
+                                <span className='icon'><i className='bi bi-linkedin'></i></span>
+                                <span className='titulo'>Linkedin</span>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
             </div>
-            
         </div>
     )
 }
